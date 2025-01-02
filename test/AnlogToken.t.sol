0932b60e21e7179f1ebf59bd3de7cae0a0c8e626
Alexander Gryaznov <hi@agryaznov.com>
2025-01-02 23:01:31 +0200

    add test_Transfer

--- test/AnlogToken.t.sol
+++ test/AnlogToken.t.sol
@@ -11,14 +11,21 @@ contract AnlogTokenTest is Test {
         token = new AnlogToken("Analog One", "ANLOG");
     }
 
-    function test_Mint() public {
+    modifier preMint(address to, uint256 amount) {
         assertEq(token.totalSupply(), 0);
+        token.mint(to, amount);
+        assertEq(token.totalSupply(), amount);
+        _;
+    }
 
-        token.mint(address(1), 20_000);
-
-        assertEq(token.totalSupply(), 20_000);
+    function test_Mint() public preMint(address(1), 20_000) {
         assertEq(token.balanceOf(address(1)), 20_000);
+    }
 
+    function test_Transfer() public preMint(address(this), 20_000) {
+        assertEq(token.balanceOf(address(2)), 0);
+        token.transfer(address(2), 5_000);
+        assertEq(token.balanceOf(address(2)), 5_000);
     }
 
     function test_Pause() public {
